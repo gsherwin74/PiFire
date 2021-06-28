ecebf503811d925c9d85ef6d6bf5518c7f306af8
gsherwin74 <gsherwin74@gmail.com>
2021-06-28 09:14:29 -0400

    Update grillplat_pifire.py
    Reversed the login for the outputs to handle high level on SSR relay

--- grillplat_pifire.py
+++ grillplat_pifire.py
@@ -27,27 +27,27 @@ class GrillPlatform:
 		for item in self.inpins:
 			GPIO.setup(self.inpins[item], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 		if GPIO.input(self.inpins['selector']) == 0:
-			GPIO.setup(self.outpins['power'], GPIO.OUT, initial=0)
-			GPIO.setup(self.outpins['igniter'], GPIO.OUT, initial=1)
-			GPIO.setup(self.outpins['fan'], GPIO.OUT, initial=1)
-			GPIO.setup(self.outpins['auger'], GPIO.OUT, initial=1)
-		else:
 			GPIO.setup(self.outpins['power'], GPIO.OUT, initial=1)
-			GPIO.setup(self.outpins['igniter'], GPIO.OUT, initial=1)
-			GPIO.setup(self.outpins['fan'], GPIO.OUT, initial=1)
-			GPIO.setup(self.outpins['auger'], GPIO.OUT, initial=1)
+			GPIO.setup(self.outpins['igniter'], GPIO.OUT, initial=0)
+			GPIO.setup(self.outpins['fan'], GPIO.OUT, initial=0)
+			GPIO.setup(self.outpins['auger'], GPIO.OUT, initial=0)
+		else:
+			GPIO.setup(self.outpins['power'], GPIO.OUT, initial=0)
+			GPIO.setup(self.outpins['igniter'], GPIO.OUT, initial=0)
+			GPIO.setup(self.outpins['fan'], GPIO.OUT, initial=0)
+			GPIO.setup(self.outpins['auger'], GPIO.OUT, initial=0)
 
 	def AugerOn(self):
-		GPIO.output(self.outpins['auger'], 0)
-
-	def AugerOff(self):
 		GPIO.output(self.outpins['auger'], 1)
 
+	def AugerOff(self):
+		GPIO.output(self.outpins['auger'], 0)
+
 	def FanOn(self):
-		GPIO.output(self.outpins['fan'], 0)
+		GPIO.output(self.outpins['fan'], 1)
 
 	def FanOff(self):
-		GPIO.output(self.outpins['fan'], 1)
+		GPIO.output(self.outpins['fan'], 0)
 
 	def FanToggle(self):
 		if(GPIO.input(self.outpins['fan']) == 0):
@@ -56,16 +56,16 @@ class GrillPlatform:
 			GPIO.output(self.outpins['fan'], 0)
 
 	def IgniterOn(self):
-		GPIO.output(self.outpins['igniter'], 0)
-
-	def IgniterOff(self):
 		GPIO.output(self.outpins['igniter'], 1)
 
+	def IgniterOff(self):
+		GPIO.output(self.outpins['igniter'], 0)
+
 	def PowerOn(self):
-		GPIO.output(self.outpins['power'], 0)
+		GPIO.output(self.outpins['power'], 1)
 
 	def PowerOff(self):
-		GPIO.output(self.outpins['power'], 1)
+		GPIO.output(self.outpins['power'], 0)
 
 	def GetInputStatus(self):
 		return (GPIO.input(self.inpins['selector']))
